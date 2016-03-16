d03e8a2bfc3c977202d6ffb52382f68072a79bb8
Kevin Mullen <kevinkozzik@gmail.com>
2016-03-16 04:14:45 -0700

    extractor hard mode

--- textminer/extractor.py
+++ textminer/extractor.py
@@ -5,3 +5,9 @@ def phone_numbers(arg):
     match = re.findall(r"(\([0-9]{3}\) ?[0-9]{3}-?[0-9]{4})", arg)
     if match:
         return match
+
+
+def emails(arg):
+    matches = re.findall(r"(([a-z]*|[a-z\.]*)@([a-z]*)\.([a-z]{2,3}))", arg)
+    if matches:
+        return [match[0] for match in matches]
